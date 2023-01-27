086d371e53ad57a16da2b3d9872b662cd6168204
ginaesps <respinosa7@ucol.mx>
2023-01-27 13:56:00 -0600

    chatController minor details

--- src/main/java/com/chatapp/chatserver/controller/ChatController.java
+++ src/main/java/com/chatapp/chatserver/controller/ChatController.java
@@ -1,4 +1,23 @@
 package com.chatapp.chatserver.controller;
 
+import com.chatapp.chatserver.controller.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatController {
+
+    //create dynamic topics in spring
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate
+
+    @MessageMapping("/private-message")
+    public Message receivePrivateMessage(@Payload Message message) {
+
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverName(), "/private", message); // /user/nombreDelUusario/private
+        return message;
+    }
 }
